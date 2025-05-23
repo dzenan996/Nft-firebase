57b299aa7e6a2a502062d1064d540e66e1e36279
dzenan996 <98219413+dzenan996@users.noreply.github.com>
2025-05-24 01:10:35 +0200

    Update firebase.ts

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,19 +1,20 @@
-// Import Firebase SDK
+// utils/firebase.ts
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase konfiguracija (iz Firebase konzole)
+// Konfiguracija iz Firebase konzole (zamijeni svojim podacima ako treba)
 const firebaseConfig = {
-  apiKey: "AIzaSyA2LFfP0bqUpyv0J73PxWsQxVQRHFJgllHo",
-  authDomain: "pinfTmarket.firebaseapp.com",
-  projectId: "pinfTmarket",
-  storageBucket: "pinfTmarket.appspot.com",
+  apiKey: "AIzaSyA2LFFP0bqUpyv0J73PxWsQxVQRHFJg1lHo",
+  authDomain: "pinftmarket.firebaseapp.com",
+  projectId: "pinftmarket",
+  storageBucket: "pinftmarket.appspot.com",
   messagingSenderId: "79815613305",
   appId: "1:79815613305:web:0d06762da5111f36d6ada8"
 };
 
-// Inicijalizacija Firebase aplikacije
+// Inicijalizacija aplikacije i baze
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// Inicijalizacija Firestore baze
-export const db = getFirestore(app);
+export { db };
